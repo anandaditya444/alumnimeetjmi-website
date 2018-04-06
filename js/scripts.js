04320f0325214cc598dc278dd20a38ee9d5fd440
anandaditya444 <anandaditya444@gmail.com>
2018-04-06 18:56:56 +0530

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+jQuery(function ($) {
+
+    'use strict';
+
+    // -------------------------------------------------------------
+    // Countdown
+    // -------------------------------------------------------------
+    (function () {
+
+        $("#back-countdiown").countdown({
+            date: "15 april 2018 09:00:00",
+            format: "on"
+        });
+    
+    }()); 
+
+
+}); // JQuery end
+
+
+$(document).on('click', '.m-menu .dropdown-menu', function(e) {
+  e.stopPropagation()
+})
